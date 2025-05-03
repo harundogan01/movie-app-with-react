9fc27c578aac90d5e0b2974d1caf4dec2da911a0
harundogan01 <harun.dogan1n@gmail.com>
2025-05-03 11:51:42 +0300

    The project has been made mobile compatible.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      '2xs': '150px',
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px'
+    }
   },
   plugins: [],
 }
