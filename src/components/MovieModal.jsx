baeb3ffec35c6c22ff67469f82a8e0691ce88b6e
harundogan01 <harun.dogan1n@gmail.com>
2025-05-03 12:21:50 +0300

    The project has been made mobile compatible for small phones.

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -10,7 +10,7 @@ const MovieModal = ({ movie, onClose }) => {
         <>
             <div className='fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50'>
                 <div className='bg-white rounded-lg shadow-lg w-full p-6 relative
-                xs:overflow-hidden xs:overflow-y-auto 2xs:w-3/4 2xs:h-auto'>
+                2xs:overflow-hidden 2xs:overflow-y-auto 2xs:w-3/4 2xs:h-[70%]'>
                     <button onClick={onClose}
                     className='absolute 2xs:top-0 2xs:text-[30px] top-4 right-4 text-gray-600 hover:text-gray-900 text-xl'>
                         &times;
